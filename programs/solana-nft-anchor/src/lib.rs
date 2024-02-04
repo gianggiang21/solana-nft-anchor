b1aca21704e360e64ae6ac0ce97bd94800be4fd8
gianggiang21 <158789293+gianggiang21@users.noreply.github.com>
2024-02-04 09:11:40 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("3ofeJa2LBdsJjQdgmEPopsSdT9y8U1KojoVsbDJdQwZY");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
